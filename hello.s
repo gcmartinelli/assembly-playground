4ef0c1393f5098e7e50c094a175e7e43637e1230
gcmartinelli <gcmartinelli@gmail.com>
2019-09-29 17:40:24 +0000

    Hello world code

---
+++ hello.s
@@ -0,0 +1,27 @@
+; Resources:
+; https://www.nasm.us/
+; http://heather.cs.ucdavis.edu/~matloff/50/LinuxAssembly.html#tth_sEc3
+; http://www.cs.fsu.edu/~langley/CNT5605/2019-Summer/assembly-example/assembly.html
+; https://blog.rchapman.org/posts/Linux_System_Call_Table_for_x86_64/
+; https://en.wikipedia.org/wiki/Write_(system_call) 
+
+; Instructions on how to compile on X86_64 Linux
+; 1) Have nasm installed
+; 2) nasm -f elf64 -o hello.o hello.s 			> compiles a hello.o object file
+; 3) ld -o hello.o hello 						> links into an executable file called hello
+; 4) ./hello 									> runs the program
+
+section .config
+	hello_world db 'Hello, world!',10 	;string and the ascii code for a new line (10)
+	global _start
+
+_start:
+	mov rax, 1 				;1 is the syscall code for write. bellow the arguments are set
+	mov rdi, 1 				;fd - 0: stdin, 1:stdou, 2:sterr
+	mov rsi, hello_world 	;buffer: char array to be written to the file pointed by fd
+	mov rdx, 14 			;length: how many bytes of the buffer to write to fd
+	syscall 				;runs the syscall set above on %rax
+
+	mov rax, 60 			;60 is the syscall code to Sys_exit. %rdi sets the error code
+	mov rdi, 0 				;error code set to 0
+	syscall
